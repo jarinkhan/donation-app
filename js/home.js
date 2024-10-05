4007a56d5c295803dd2f56243db34d191cd743d2
jarinkhan <jarin7khan@gmail.com>
2024-10-05 12:50:30 +0600

    partially added the add money event

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
-console.log('home added');
+document.getElementById('btn-add-money')
+.addEventListener('click',function(event){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    const pinNum = document.getElementById('input-pin-num').value;
+    console.log(pinNum); 
+
+
+
+});
